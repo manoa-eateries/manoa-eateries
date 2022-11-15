c3559bdcb762f4331d1ed1c2be7b7ca0108634ea
brookemaeda <bmhm@hawaii.edu>
2022-11-15 10:04:17 -1000

    updated issue-01 - landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
@@ -9,7 +9,12 @@ const Landing = () => (
         <h1>Manoa Eateries</h1>
         <p>Manoa Eateries gives students the opportunity to see what is available to eat on campus.  Users can view various food options based on their preferences, and vendors can update their menu items and operating house.</p>
         <h3>PARTICIPATING VENDORS</h3>
-        <p>Dunkin' Donuts, Holoholo Grill, L&L Hawaiian Barbecue, Le Crepe Cafe, Lasoon, Panda Express, Ahi & Vegetable, Happy Belly Eatery, HI Cravings, Holoholo Bistro, Raysam Hawaii, Slice By HB Baking, The Bean Counter, Bale, Campus Center Food Court, Gateway Cafe, Hale Aloha Cafe, Jamba Juice, Starbucks, B'RITO, The Market.</p>
+        <p>
+          Dunkin`&apos;` Donuts, Holoholo Grill, L&L Hawaiian Barbecue, Le Crepe Cafe, Lasoon,
+          Panda Express, Ahi & Vegetable, Happy Belly Eatery, HI Cravings, Holoholo Bistro,
+          Raysam Hawaii, Slice By HB Baking, The Bean Counter, Bale, Campus Center Food Court,
+          Gateway Cafe, Hale Aloha Cafe, Jamba Juice, Starbucks, B`&apos;`RITO, The Market.
+        </p>
       </Container>
     </Row>
   </Container>
