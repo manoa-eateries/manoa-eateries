717fd48e31e0b031a40f3a07832e7d20daa9cf70
Eliya Nakamura <eliyanak@hawaii.edu>
2022-11-17 18:24:25 -1000

    Fixed logo image and updated background height

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { NavLink } from 'react-router-dom';
-import logo from 'images/manoa_eateries_green.png';
 import { Container, Nav, Navbar, NavDropdown, Image } from 'react-bootstrap';
 
 const NavBar = () => {
@@ -12,40 +11,40 @@ const NavBar = () => {
   }), []);
 
   return (
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand as={NavLink} to="/">
-              <Image src={logo} />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="justify-content-end">
-              {currentUser === '' ? (
-                  <NavDropdown id="login-dropdown" title="Login">
-                    <NavDropdown.Item id="login-dropdown-sign-in" as={NavLink} to="/signin">
-                      Sign
-                      in
-                    </NavDropdown.Item>
-                    <NavDropdown.Item id="login-dropdown-sign-up" as={NavLink} to="/signup">
-                      Sign
-                      up
-                    </NavDropdown.Item>
-                  </NavDropdown>
-              ) : (
-                  <NavDropdown id="account" title="Account">
-                    <NavDropdown.Item id="profile" as={NavLink} to="/Profile">
-                      Profile
-                    </NavDropdown.Item>
-                    <NavDropdown.Item id="signout" as={NavLink} to="/signout">
-                      Sign
-                      out
-                    </NavDropdown.Item>
-                  </NavDropdown>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand as={NavLink} to="/">
+          <Image src="images/manoa_eateries_green.png" width="100px" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+          <Nav>
+            {currentUser === '' ? (
+              <NavDropdown id="login-dropdown" title="Login">
+                <NavDropdown.Item id="login-dropdown-sign-in" as={NavLink} to="/signin">
+                  Sign
+                  in
+                </NavDropdown.Item>
+                <NavDropdown.Item id="login-dropdown-sign-up" as={NavLink} to="/signup">
+                  Sign
+                  up
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <NavDropdown id="account" title="Account">
+                <NavDropdown.Item id="profile" as={NavLink} to="/Profile">
+                  Profile
+                </NavDropdown.Item>
+                <NavDropdown.Item id="signout" as={NavLink} to="/signout">
+                  Sign
+                  out
+                </NavDropdown.Item>
+              </NavDropdown>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
