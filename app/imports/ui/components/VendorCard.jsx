e0a4f1ce5aea133bc2925928d07b7dab86d703c0
brookemaeda <bmhm@hawaii.edu>
2022-11-28 20:59:00 -1000

    updated issue - 18 - list vendors

--- app/imports/ui/components/VendorCard.jsx
+++ app/imports/ui/components/VendorCard.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import { Image, Card } from 'react-bootstrap';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
