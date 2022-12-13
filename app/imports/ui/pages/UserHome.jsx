0089cd108d1d25c19a2f9dfbaf5128ee277cde26
brookemaeda <bmhm@hawaii.edu>
2022-12-12 19:53:07 -1000

    update issue 16 - userhome

--- app/imports/ui/pages/UserHome.jsx
+++ app/imports/ui/pages/UserHome.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Col, Container, Row } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
+import { _ } from 'meteor/underscore';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { VendorProfiles } from '../../api/vendor/vendorProfile';
 import VendorCard from '../components/VendorCard';
@@ -9,11 +10,11 @@ import VendorCard from '../components/VendorCard';
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 const UserHome = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
-  const { ready, vendors } = useTracker(() => {
+  const { vendors, ready } = useTracker(() => {
     // Note that this subscription will get cleaned up
     // when your component is unmounted or deps change.
     // Get access to Stuff documents.
-    const subscription = Meteor.subscribe(VendorProfiles.userPublicationName);
+    const subscription = Meteor.subscribe(VendorProfiles.adminPublicationName);
     // Determine if the subscription is ready
     const rdy = subscription.ready();
     // Get the Stuff documents
@@ -23,15 +24,19 @@ const UserHome = () => {
       ready: rdy,
     };
   }, []);
+  let now = new Date();
+  now = now.getHours();
+  const selectVendors = _.filter(vendors, function (vendor) { return now > vendor.openHour && now < vendor.closeHour; });
+  console.log(`todays date is ${now}`);
   return (ready ? (
-    <Container className="py-3">
+    <Container className="py-3" id="list-vendors-page">
       <Row className="justify-content-center">
         <Col md={7}>
           <Col className="text-center">
-            <h2>List Vendors</h2>
+            <h2>Open Now</h2>
           </Col>
           <Row>
-            {vendors.map((vendor) => (<Col key={vendor._id}><VendorCard vendor={vendor} /></Col>))}
+            {selectVendors.map((vendor) => (<Col key={vendor._id}><VendorCard vendor={vendor} /></Col>))}
           </Row>
         </Col>
       </Row>
